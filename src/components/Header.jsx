bde6acadeda7318d9fb3cea400557a2a63094b03
Denis Polyakov <pdd1328@gmail.com>
2023-10-11 13:20:48 +0300

    First commit.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2I75rxUUGe78wcrBLRtUg5NAoqSCJf88-Tg&usqp=CAU"></img>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://images.unsplash.com/photo-1417577097439-425fb7dec05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
