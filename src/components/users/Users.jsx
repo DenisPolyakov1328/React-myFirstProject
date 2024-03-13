9d05568daa5d9501fe09a8c414b7b81f2b80d6ba
Denis Polyakov <pdd1328@gmail.com>
2024-03-13 12:32:08 +0300

    убрали side effect, теперь функция/компонента не изменяет внешний мир

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -3,16 +3,19 @@ import axios from "axios";
 import userPhoto from "../../assets/image/user.png";
 
 const Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => {
-        props.setUsers(response.data.items);
-      });
-  }
+  const getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
   return (
     <div>
+      <button onClick={getUsers}>Get users</button>
       {props.users.map((user) => (
         <div key={user.id}>
           <span>
