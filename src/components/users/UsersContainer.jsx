c1d59d97d2e8eca98e1f209de95eeb0342f53ea6
Denis Polyakov <pdd1328@gmail.com>
2024-03-20 18:37:37 +0300

    Сделал контейнерную компоненту(с запросом на сервер) внутри контейнерной(которая "общается" со стейтом), компонента User теперь чистая/презентационная

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -1,6 +1,52 @@
+import React from "react";
 import { connect } from "react-redux";
 import Users from "./Users";
-import {followActionCreator, unfollowActionCreator, setUsersActionCreator, setCurrentPageActionCreator, setUsersTotalCountActionCreator } from './../../Redux/users-reducer'
+import {
+  followActionCreator,
+  unfollowActionCreator,
+  setUsersActionCreator,
+  setCurrentPageActionCreator,
+  setUsersTotalCountActionCreator,
+} from "./../../Redux/users-reducer";
+import axios from "axios";
+
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPostChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <Users
+        totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage}
+        onPostChanged={this.onPostChanged}
+        users={this.props.users}
+        follow={this.props.follow}
+        unfollow={this.props.unfollow}
+      />
+    );
+  }
+}
 
 // Вызывается при каждом изменении состояние хранилища(store).Получает всё состояние хранилища(store) и возвращает объект с необходимыми этому компоненту данными (пропсами).
 const mapStateToProps = (state) => {
@@ -8,30 +54,28 @@ const mapStateToProps = (state) => {
     users: state.usersPage.users,
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage
-  }
+    currentPage: state.usersPage.currentPage,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
-      dispatch(followActionCreator(userId))
+      dispatch(followActionCreator(userId));
     },
     unfollow: (userId) => {
-      dispatch(unfollowActionCreator(userId))
+      dispatch(unfollowActionCreator(userId));
     },
     setUsers: (users) => {
-      dispatch(setUsersActionCreator(users))
+      dispatch(setUsersActionCreator(users));
     },
     setCurrentPage: (pageNumber) => {
-      dispatch(setCurrentPageActionCreator(pageNumber))
+      dispatch(setCurrentPageActionCreator(pageNumber));
     },
     setTotalUsersCount: (totalCount) => {
-      dispatch(setUsersTotalCountActionCreator(totalCount))
-    }
-  }
+      dispatch(setUsersTotalCountActionCreator(totalCount));
+    },
+  };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps) (Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
